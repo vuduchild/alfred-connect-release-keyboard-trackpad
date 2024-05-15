0db94e9a2a027ff7ce22edde3d054f3654d487ae
Roy Rothenberg <royrothenberg@gmail.com>
2024-05-15 12:35:47 -0400

    refactoring to use remote ssh to connect devices

---
+++ disconnect_local_bluetooth_device_and_connect_it_remotely.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+bluetooth_device_mac=$1
+remote_user=$2
+remote_machine=$3
+blueutil=/opt/homebrew/bin/blueutil
+
+echo "Disconnecting device ${bluetooth_device_mac} locally"
+${blueutil} --unpair ${bluetooth_device_mac} &
+
+echo "Sshing to ${remote_user}@${remote_machine} to connect device $bluetooth_device_mac"
+ssh "${remote_user}@${remote_machine}" "${blueutil} --pair ${bluetooth_device_mac} && sleep 0.3 && ${blueutil} --connect ${bluetooth_device_mac}"
