32a34c476588a76af305c022f3938f5843e24cf0
Roy Rothenberg <royrothenberg@gmail.com>
2024-05-15 13:06:38 -0400

    increasing sleep between blueutil invocations

--- disconnect_local_bluetooth_device_and_connect_it_remotely.sh
+++ disconnect_local_bluetooth_device_and_connect_it_remotely.sh
@@ -9,4 +9,4 @@ echo "Disconnecting device ${bluetooth_device_mac} locally"
 ${blueutil} --unpair ${bluetooth_device_mac} &
 
 echo "Sshing to ${remote_user}@${remote_machine} to connect device $bluetooth_device_mac"
-ssh "${remote_user}@${remote_machine}" "${blueutil} --pair ${bluetooth_device_mac} && sleep 0.3 && ${blueutil} --connect ${bluetooth_device_mac}"
+ssh "${remote_user}@${remote_machine}" "${blueutil} --pair ${bluetooth_device_mac} && sleep 1 && ${blueutil} --connect ${bluetooth_device_mac}"
