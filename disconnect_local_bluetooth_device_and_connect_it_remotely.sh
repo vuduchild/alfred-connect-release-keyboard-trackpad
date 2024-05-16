1b17d24bb1f0a074c964edb0481e834d0f5e9267
Roy Rothenberg <royrothenberg@gmail.com>
2024-05-16 15:36:35 -0400

    check if device is connected before (un)pairing to avoid hangs

--- disconnect_local_bluetooth_device_and_connect_it_remotely.sh
+++ disconnect_local_bluetooth_device_and_connect_it_remotely.sh
@@ -6,7 +6,7 @@ remote_machine=$3
 blueutil=/opt/homebrew/bin/blueutil
 
 echo "Disconnecting device ${bluetooth_device_mac} locally"
-${blueutil} --unpair ${bluetooth_device_mac} &
+(${blueutil} --is-connected ${bluetooth_device_mac} && ${blueutil} --unpair ${bluetooth_device_mac}) &
 
 echo "Sshing to ${remote_user}@${remote_machine} to connect device $bluetooth_device_mac"
-ssh "${remote_user}@${remote_machine}" "${blueutil} --pair ${bluetooth_device_mac} && sleep 1 && ${blueutil} --connect ${bluetooth_device_mac}"
+ssh "${remote_user}@${remote_machine}" "${blueutil} --is-connected ${bluetooth_device_mac} || (${blueutil} --pair ${bluetooth_device_mac} && sleep 1 && ${blueutil} --connect ${bluetooth_device_mac})"
