86cae8f5c95e8cb13ff09f49423002a860b93169
Roy Rothenberg <royrothenberg@gmail.com>
2024-05-20 11:36:22 -0400

    add gtimeout

--- disconnect_local_bluetooth_device_and_connect_it_remotely.sh
+++ disconnect_local_bluetooth_device_and_connect_it_remotely.sh
@@ -3,10 +3,13 @@
 bluetooth_device_mac=$1
 remote_user=$2
 remote_machine=$3
-blueutil=/opt/homebrew/bin/blueutil
+homebrew_path=/opt/homebrew/bin
+blueutil="${homebrew_path}/blueutil"
+gtimeout="${homebrew_path}/gtimeout"
 
 echo "Disconnecting device ${bluetooth_device_mac} locally"
-(${blueutil} --is-connected ${bluetooth_device_mac} && ${blueutil} --unpair ${bluetooth_device_mac}) &
+# --is-connected returns 1 if connected and 0 if not. Yes, I know. WTF
+${blueutil} --is-connected ${bluetooth_device_mac} && ${blueutil} --unpair ${bluetooth_device_mac}
 
 echo "Sshing to ${remote_user}@${remote_machine} to connect device $bluetooth_device_mac"
-ssh "${remote_user}@${remote_machine}" "${blueutil} --is-connected ${bluetooth_device_mac} || (${blueutil} --pair ${bluetooth_device_mac} && sleep 1 && ${blueutil} --connect ${bluetooth_device_mac})"
+ssh "${remote_user}@${remote_machine}" "${blueutil} --is-connected ${bluetooth_device_mac} || ${gtimeout} 4 ${blueutil} --pair ${bluetooth_device_mac} && sleep 1 && ${gtimeout} 4 ${blueutil} --connect ${bluetooth_device_mac}"
